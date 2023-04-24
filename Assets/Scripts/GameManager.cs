bbed30c3de4cc9d735e25d5250eb2f51fefa4f62
MaxLepan <max.lepan@laposte.net>
2023-04-25 00:41:24 +0200

    Added high score management

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,18 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] TextMeshPro scoreText;
+    [SerializeField] TextMeshPro highScoreText;
     [SerializeField] float timeBeforeRestart = 2;
     [SerializeField] float slowness = 10;
     [SerializeField] AudioSource audioSource;
 
     bool isGameEnded = false;
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt("high_score");
+        highScoreText.SetText(highScore.ToString("0"));
+    }
 
     private void Update()
     {
@@ -19,6 +26,10 @@ public class GameManager : MonoBehaviour
         {
             float score = Time.timeSinceLevelLoad * 10f;
             scoreText.SetText(score.ToString("0"));
+            if (score > float.Parse(highScoreText.text))
+            {
+                highScoreText.SetText(score.ToString("0"));
+            }
         }
     }
 
@@ -33,6 +44,8 @@ public class GameManager : MonoBehaviour
 
     IEnumerator RestartLevel()
     {
+        PlayerPrefs.SetInt("high_score", int.Parse(highScoreText.text));
+
         Time.timeScale = 1f / slowness;
         Time.fixedDeltaTime = Time.fixedDeltaTime / slowness;
         audioSource.pitch = audioSource.pitch / slowness * 5;
